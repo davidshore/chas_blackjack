eb598aa9e1529a50a31ba60bea66e92c1a2fb9b7
chasacademy-EmmaJohansson <114146126+chasacademy-EmmaJohansson@users.noreply.github.com>
2022-12-19 19:34:18 +0100

    ok

--- code.js
+++ code.js
@@ -12,45 +12,24 @@ const resultElement = document.querySelector("#result");
 let sum = 0;
 let dealerCardSum = 0;
 
-// Delar i blackjack-spel
-// - Ta ett slumpat kort
-// - Hålla reda på summan av tagna kort ( variabel med användarens summa )
-// - Om summan är över 21 så skriv på skärmen att du förlorade.
-// - Stoppknapp - klar och dealern kan börja ta kort.
-// - Dealerns kort - while loop - loopa till att dealern får över eller lika med 17
-// - Håll reda på dealerns summa av kort för att vet när du ska stanna while-loopen
-// // let dealerCardSum = 0
-// // while( dealerCardSum <= 17 )
-// - När while-loopen stannar - kolla vem som vann.
-
-//1. Ta fram en slupad siffra mellan 0 och kortlekens längd.
-
-// const randomNum1 = Math.random(); // 0 -> 0.99999
-// const randomNum2 = randomNum1 * cards.length; // 0 -> 51.99999
-// const randomNum3 = Math.floor(randomNum2); // 0 -> 51
-
 function getRandomCard() {
   const randomNum = Math.floor(Math.random() * cards.length);
-
-  // const randomCard = cards[randomNum];
   const randomCard = currentCards.splice(randomNum, 1);
 
   return randomCard[0];
 }
 
 function createCard(parentElement) {
-  const card = getRandomCard(); // {num: ... , file: ...  };
+  const card = getRandomCard();
 
-  const imgTag = document.createElement("img"); // <img />
-  imgTag.src = `images/${card.file}`; // <img src="..." />
+  const imgTag = document.createElement("img");
+  imgTag.src = `images/${card.file}`;
   imgTag.width = "100";
   imgTag.height = "145";
 
   parentElement.appendChild(imgTag);
   return card;
 }
-
-// funktionen ska ta ett kort och uppdatera summan
 function userTakeCard(event) {
   const card = createCard(cardsElement);
   sum += card.num;
